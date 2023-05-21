0ccd538cc0a86a898e2fb9784e9df85bfef57dad
Jamshidbek <coder.jamshidbek@gmail.com>
2023-05-21 20:21:00 +0200

    explanation/usleep, added struct as comment

--- explanation/usleep.c
+++ explanation/usleep.c
@@ -2,6 +2,11 @@
 #include <sys/time.h> // time
 #include <unistd.h> // usleep
 
+// struct timeval {
+//     time_t      tv_sec;     // seconds
+//     suseconds_t tv_usec;    // microseconds
+// };
+
 void    example()
 {
     struct timeval  start_time;
